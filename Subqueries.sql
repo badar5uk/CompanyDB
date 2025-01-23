a190d58f0067b7a29e70140a5a1752e5be4cdf52
Badar <bader5uk@hotmail.com>
2025-01-23 13:09:34 +0400

    further edits to queries

--- Subqueries.sql
+++ Subqueries.sql
@@ -14,7 +14,19 @@ Find employees whose salaries are greater than the average salary of their respe
 */
 SELECT *
 FROM employees g1
-WHERE g1.salary > (select avg(g2.salary) from employees g2 where g2.department_id = g1.department_id);
+WHERE g1.salary > (select avg(g2.salary) from employees g2 where g2.department_id = g1.department_id)
+group by g1.department_id;
+
+SELECT e.name, e.Salary, e.department_id
+FROM employees e
+WHERE e.Salary > (
+    SELECT AVG(Salary)
+    FROM employees sub
+    WHERE sub.department_id = e.department_id
+) OR e.department_id NOT IN (
+    SELECT DISTINCT department_id
+    FROM employees
+);
 
 /*
 Employees with no dpeartments
@@ -31,10 +43,11 @@ SELECT project_name from projects WHERE department_id = null;
 Department with highest number of employees
 */
 
-SELECT y.department_id, MAX(y.c) as "num of emp"
-FROM (SELECT e.department_id as "department_id", COUNT(e.employee_id) as "c" FROM employees e GROUP BY e.department_id) y
-GROUP BY y.department_id 
-
+SELECT TOP 1 y.department_id, y.c AS "num_of_emp"
+FROM (SELECT e.department_id, COUNT(e.employee_id) AS c
+FROM employees e
+    GROUP BY e.department_id) y
+ORDER BY y.c DESC;
 /*
 Names and salaries of highest paid in each department
 */
@@ -94,3 +107,54 @@ Display the department ID and the total number of employees in each department.
 SELECT department_id, COUNT(*) as 'Total Employees'
 FROM employees
 GROUP BY department_id;
+
+/*
+List employees who are in departments that do not have any projects.
+*/
+SELECT *
+FROM employees e 
+WHERE e.department_id not in (SELECT department_id FROM projects)
+
+/*
+List the names of projects that have a budget higher than the average project budget.
+*/
+
+SELECT project_name
+FROM projects
+WHERE budget > (SELECT AVG(budget) FROM projects);
+
+/*
+Find the department IDs of departments that have fewer than three employees.
+*/
+
+SELECT department_id
+FROM employees
+GROUP BY department_id
+HAVING COUNT(employee_id) < 3
+
+/*
+Find the name of the employee with the highest salary in the company
+*/
+
+SELECT name
+FROM employees
+WHERE Salary = (SELECT MAX(Salary) FROM employees)
+
+/*
+Find the names of departments that manage projects with a budget of less than $100,000.
+*/
+
+SELECT d.department_name, d.department_id FROM
+departments d
+WHERE d.department_id in (SELECT department_id from projects p
+Where budget < 100000)
+GROUP BY d.department_name,d.department_id;
+
+/*
+Identify the name and hire date of the most recently hired employee.
+*/
+SELECT name, hire_date
+FROM employees
+WHERE hire_date in (SELECT max(hire_date) FROM employees)
+
+
