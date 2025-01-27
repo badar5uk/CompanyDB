5fc9ef4fea8b01336c5dbe44da7aa2c38485e52b
Badar <bader5uk@hotmail.com>
2025-01-27 08:49:24 +0400

    Removed WHERE clause

--- Cross_Join_queries.sql
+++ Cross_Join_queries.sql
@@ -8,7 +8,6 @@ Write a query to generate a list of all possible employee-project pairs.
 SELECT *
 FROM employees e
 CROSS JOIN projects p
-WHERE e.department_id = p.department_id
 
 /*
 Generate a List of Department-Project Pairs
@@ -17,4 +16,3 @@ Write a query to pair each department with every project in the database.
 SELECT *
 FROM departments d
 CROSS JOIN projects p
-WHERE d.department_id = p.department_id
